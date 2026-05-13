d8e60aa2c26ddc776ba6bb6ce84bfc79c8029775
Maxie Bichmann <maxie.bichmann@tum.de>
2026-05-14 00:45:54 +0200

    start kls

--- src/algorithms/lc_css_kls.py
+++ src/algorithms/lc_css_kls.py
@@ -7,6 +7,66 @@ import ldpc.mod2.mod2_numpy as mod2
 import pyzx as zx
 from ..core.stabilizer_code import StabilizerCode
 
+class ZXGraph:
+    def __init__(self):
+        # each node is represented as its phase-factor of pi, input nodes before output nodes
+        self.n = 0
+        self.k = 0
+        self.vertices = []
+
+        self.edges = set()
+
+    def add_edge(self, idx1: float, idx2: float):
+        self.edges.add({ idx1, idx2 })
+
+    def add_input_vertex(self, u: float):
+        self.vertices.append(u)
+        self.k += 1
+        self.n += 1
+
+    def add_output_vertex(self, u: float):
+        self.vertices.append(u)
+        self.n += 1
+
+    def get_input_vertices(self) -> list[float]:
+        return self.vertices[:self.k]
+    
+    def get_output_vertices(self) -> list[float]:
+        return self.vertices[self.k:]
+    
+    def get_input_output_adjacency(self) -> np.ndarray:
+        adj = np.zeros((self.k, self.n), dtype=bool)
+        for edge in self.edges:
+            u, v = edge
+            if u < self.k and v >= self.k:
+                adj[u, v - self.k] = True
+                adj[v - self.k, u] = True
+            elif v < self.k and u >= self.k:
+                adj[v, u - self.k] = True
+                adj[u - self.k, v] = True
+        return adj
+    
+    def get_full_adjacency(self) -> np.ndarray:
+        adj = np.zeros((self.n + self.k, self.n + self.k), dtype=bool)
+        for edge in self.edges:
+            u, v = edge
+            i = self.vertices.index(u)
+            j = self.vertices.index(v)
+            adj[i, j] = True
+            adj[j, i] = True
+        return adj
+    
+    def local_complementation(self, i: int):
+        neighbors = [v for v in self.vertices if {i, v} in self.edges]
+        for i in range(len(neighbors)):
+            for j in range(i + 1, len(neighbors)):
+                u = neighbors[i]
+                v = neighbors[j]
+                if {u, v} in self.edges:
+                    self.edges.remove({u, v})
+                else:
+                    self.edges.add({u, v})
+
 def _code_to_encoder_circuit(code) -> zx.Circuit:
     def _delete_first_row_and_qubit(tab: np.ndarray) -> np.ndarray:
         n = tab.shape[1] // 2
@@ -53,8 +113,9 @@ def _code_to_encoder_circuit(code) -> zx.Circuit:
             raise RuntimeError("Failed to convert stabilizer row to Z support.")
         pivot = int(z_support[0])
 
-        tableau[:, pivot] ^= tableau[:, 0]
-        tableau[:, cur_n + 0] ^= tableau[:, cur_n + pivot]
+        if pivot != 0:
+            tableau[:, pivot] ^= tableau[:, 0]
+            tableau[:, cur_n + 0] ^= tableau[:, cur_n + pivot]
 
         # 3.) clear all other Zs in row 0 using CNOT(q -> 0)
         # control: (x_c|z_c) --CNOT--> (  x_c  |z_c^z_t)
@@ -102,7 +163,7 @@ def _code_to_encoder_circuit(code) -> zx.Circuit:
         
     return circuit 
 
-def _code_to_graph_state(code):
+def _code_to_graph(code) -> ZXGraph:
     # 1.) code -> encoder circuit
     circuit = _code_to_encoder_circuit(code)
 
@@ -112,20 +173,80 @@ def _code_to_graph_state(code):
     # 3.) zx diagram -> graph state
     zx_diagram.normalize() 
 
-    pass
+    # 4.) graph state -> ZXGraph
+    # TODO
+    graph = ZXGraph()
 
-def _encoder_circuit_to_graph_state(circuit):
-    circuit = zx.Circuit.to_basic_gates(circuit)
-    zx_diagram = circuit.to_graph()
-    zx_diagram.normalize() 
-    pass
+    print(zx_diagram)
 
-def _hk_normal_form(graph_state):
-    pass
+def _hk_normal_form(graph: ZXGraph) -> ZXGraph:
+    # TODO
+    return graph
 
-def _kls_normal_form(hk_normal_form):
-    pass
+def _kls_normal_form(graph_hk: ZXGraph) -> ZXGraph:
+    # 1.) set input vertices to have no phase
+    for q in range(graph_hk.k):
+        graph_hk.vertices[q] = 0
+
+    # 2.) bring the IO-adjacency matrix into RREF
+    adj = graph_hk.get_input_output_adjacency()
+    gates = []
+    for i in range(graph_hk.k):
+        pivot_candidates = np.flatnonzero(adj[i, :])
+        if len(pivot_candidates) == 0:
+            continue
+        pivot = int(pivot_candidates[0])
+        for r in range(0, graph_hk.k):
+            if r != i and adj[r, pivot]:
+                adj[r] ^= adj[i]
+                gates.append(("CNOT", (i, r)))
+
+    # 3.) simplify the graph with the added gates
+    zx_diagram = zx.Graph()
+    for p in graph_hk.vertices:
+        print(p)
+
+    for u, v in graph_hk.edges:
+        print(u, v)
+
+    zx_diagram.normalize()
+
+    # 4.) graph state -> ZXGraph
+    # TODO
+    graph = ZXGraph()
+
+    return graph
+
+def _is_bipartite(graph: ZXGraph) -> bool:
+    adj = graph.get_full_adjacency()
+    n = adj.shape[0]
+    colors = np.full(n, -1, dtype=np.int8)
+
+    for start in range(n):
+        if colors[start] != -1:
+            continue
+
+        colors[start] = 0
+        frontier = np.array([start], dtype=int)
+
+        while frontier.size:
+            current_color = colors[frontier[0]]
+            next_color = 1 - current_color
+
+            neighbors = np.flatnonzero(adj[frontier].any(axis=0))
+
+            if np.any(colors[neighbors] == current_color):
+                return False
+
+            new = neighbors[colors[neighbors] == -1]
+            colors[new] = next_color
+            frontier = new
+
+    return True
 
 
 def is_lceq_css_kls(code: StabilizerCode) -> bool:
-    return False
+    graph = _code_to_graph(code)
+    graph_hk = _hk_normal_form(graph)
+    graph_kls = _kls_normal_form(graph_hk)
+    return _is_bipartite(graph_kls)
