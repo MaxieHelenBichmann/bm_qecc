e5bf8b51f7f102a0ebaf612b119064d9c3e9abfd
Maxie Bichmann <maxie.bichmann@tum.de>
2026-05-20 21:46:51 +0200

    add tests lc eq

--- tests/lc_eq/test_lc_eq_graph_state.py
+++ tests/lc_eq/test_lc_eq_graph_state.py
@@ -7,9 +7,10 @@ import pytest
 
 from benchmarks.utils import random_stabilizer_code, lc_equivalent_code
 from src.algorithms.lc_eq_graph_state import (
+    _code_to_encoder_circuit,
+    _stab_state_to_graph_state,
+    _code_to_graph,
     _lc_equiv_graph_states,
-    #_stab_code_to_stab_state,
-    #_stab_state_to_graph_state,
     are_lceq_graph_state,
 )
 from src.core.stabilizer_code import StabilizerCode
@@ -18,77 +19,13 @@ def _assert_same_matrix(actual: np.ndarray, expected: np.ndarray) -> None:
     np.testing.assert_array_equal(actual.astype(np.uint8), expected.astype(np.uint8))
 
 # ----------------------------------------------------------------------------------------------------
-# _stab_code_to_stab_state
+# _code_to_encoder_circuit
 # ----------------------------------------------------------------------------------------------------
 
-@pytest.mark.parametrize(
-    ("code", "expected"),
-    [
-        pytest.param(
-            StabilizerCode(["Z"]),
-            np.array([[0, 1]], dtype=np.uint8),
-            id="single-qubit-z-state",
-        ),
-        pytest.param(
-            StabilizerCode(["X"]),
-            np.array([[1, 0]], dtype=np.uint8),
-            id="single-qubit-x-state",
-        ),
-        pytest.param(
-            StabilizerCode.get_trivial_code(1),
-            np.array(
-                [
-                    [1, 1, 0, 0],
-                    [0, 0, 1, 1],
-                ],
-                dtype=np.uint8,
-            ),
-            id="one-qubit-trivial-code",
-        ),
-        pytest.param(
-            StabilizerCode(["ZZ"], z_logicals=["ZI"], x_logicals=["XX"]),
-            np.array(
-                [
-                    [0, 0, 0, 1, 1, 0],
-                    [1, 1, 1, 0, 0, 0],
-                    [0, 0, 0, 1, 0, 1],
-                ],
-                dtype=np.uint8,
-            ),
-            id="two-qubit-repetition-code",
-        ),
-        pytest.param(
-            StabilizerCode(["ZZI", "IZZ"], z_logicals=["ZII"], x_logicals=["XXX"]),
-            np.array(
-                [
-                    [0, 0, 0, 0, 1, 1, 0, 0],
-                    [0, 0, 0, 0, 0, 1, 1, 0],
-                    [1, 1, 1, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 0, 0, 1],
-                ],
-                dtype=np.uint8,
-            ),
-            id="three-qubit-repetition-code",
-        ),
-        pytest.param(
-            StabilizerCode(["XZYI", "IXXY"]),
-            np.array(
-                [
-                    [1, 0, 1, 0, 0, 0,  0, 1, 1, 0, 0, 0],
-                    [0, 1, 1, 1, 0, 0,  0, 0, 0, 1, 0, 0],
-                    [0, 0, 0, 1, 1, 0,  0, 1, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 1,  1, 0, 0, 0, 0, 0],
-                    [0, 1, 1, 0, 0, 0,  0, 0, 0, 0, 1, 0],
-                    [1, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 1],
-                ],
-                dtype=np.uint8,
-            ),
-            id="log-op-generators",
-        ),
-    ],
-)
-def utest_stab_code_to_stab_state_small_codes(code: StabilizerCode, expected: np.ndarray) -> None:
-    _assert_same_matrix(_stab_code_to_stab_state(code), expected)
+# ----------------------------------------------------------------------------------------------------
+# _code_to_graph
+# ----------------------------------------------------------------------------------------------------
+
 
 # ----------------------------------------------------------------------------------------------------
 # _stab_state_to_graph_state
@@ -141,7 +78,7 @@ def utest_stab_code_to_stab_state_small_codes(code: StabilizerCode, expected: np
         ),
     ],
 )
-def utest_stab_state_to_graph_state_small_tableau(
+def test_stab_state_to_graph_state_small_tableau(
     tableau: np.ndarray,
     expected: np.ndarray,
 ) -> None:
