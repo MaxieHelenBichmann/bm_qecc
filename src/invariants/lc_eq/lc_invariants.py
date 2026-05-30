74c895d09bfd560479d8de227341e7c000ec4fa4
Maxie Bichmann <maxie.bichmann@tum.de>
2026-05-30 22:22:11 +0200

    add scaffold

--- src/invariants/lc_eq/lc_invariants.py
+++ src/invariants/lc_eq/lc_invariants.py
@@ -14,3 +14,19 @@ def preserved_n(c1: StabilizerCode, c2: StabilizerCode) -> bool:
 def preserved_k(c1: StabilizerCode, c2: StabilizerCode) -> bool:
     """Check whether the number of logical qubits is preserved, which is a necessary condition for LC-equivalence."""
     return c1.k == c2.k
+
+def preserved_local_weight_distribution(c1: StabilizerCode, c2: StabilizerCode) -> bool:
+    """Check whether the local weight distribution is preserved, which is a necessary condition for LC-equivalence.
+    
+    Reference for this invariant: 
+    - Maarten Van den Nest, Jeroen Dehaene, Bart De Moor: Finite set of invariants to characterize local Clifford equivalence of stabilizer states
+    """
+    return False
+
+def preserved_low_degree_local_invariant(c1: StabilizerCode, c2: StabilizerCode) -> bool:
+    """Check whether the a low degree local invariant is preserved, which is a necessary condition for LC-equivalence.
+    
+    Reference for this invariant: 
+    - Maarten Van den Nest, Bart De Moor: Local Invariants of Stabilizer Codes
+    """
+    return False
