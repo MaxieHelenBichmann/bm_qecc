1971690a4c26dd5c7bf64e521a1cd04687c7fc1c
Maxie Bichmann <maxie.bichmann@tum.de>
2026-09-06 17:14:32 +0200

    modular lc stb sat

--- src/algorithms/lc_stb/lc_stb_sat.py
+++ src/algorithms/lc_stb/lc_stb_sat.py
@@ -22,15 +22,10 @@ def _xor_list(variables):
     return acc
 
 
-def are_lceq_sat(c1: StabilizerCode, c2: StabilizerCode) -> bool:
-    """Check local-clifford equivalence by reducing it to a SAT problem and using a SAT solver.
-
-    The idea is to create boolean variables for the local clifford operations and the row operations, and then add constraints that enforce that the local-clifford tableau of c1 is equal to the row-operated tableau of c2.
-    1.) Create a auxiliary tableau that encodes the local clifford operations of c1, and the row operations of c2.
-    2.) Create boolean variables c_{i,j} for the local cliffords, where c_{i,j} is true if the i-th local clifford operation (I, H, S, HS, SH, HSH)is applied to the j-th qubit of c1 in the auxiliary tableau.
-    3.) Create boolean variables r_{i,j} for the row operations, where r_{i,j} is true if the j-th row is added to the i-th row in the auxiliary tableau.
-    4.) Check satisfiability of the resulting formula. If it is satisfiable, then c1 and c2 are local clifford equivalent, otherwise they are not.
-    """
+def _build_lceq_stab_sat_solver(
+    c1: StabilizerCode, c2: StabilizerCode
+) -> z3.Solver:
+    """Build the SAT instance."""
     solver = z3.Solver()
 
     n = c1.n
@@ -83,4 +78,16 @@ def are_lceq_sat(c1: StabilizerCode, c2: StabilizerCode) -> bool:
 
             solver.add(aux_tableau[row * (2*n) + q] == _xor_list(row_contributions))
 
-    return solver.check() == z3.sat
+    return solver
+
+
+def are_lceq_sat(c1: StabilizerCode, c2: StabilizerCode) -> bool:
+    """Check local-clifford equivalence by reducing it to a SAT problem and using a SAT solver.
+
+    The idea is to create boolean variables for the local clifford operations and the row operations, and then add constraints that enforce that the local-clifford tableau of c1 is equal to the row-operated tableau of c2.
+    1.) Create a auxiliary tableau that encodes the local clifford operations of c1, and the row operations of c2.
+    2.) Create boolean variables c_{i,j} for the local cliffords, where c_{i,j} is true if the i-th local clifford operation (I, H, S, HS, SH, HSH)is applied to the j-th qubit of c1 in the auxiliary tableau.
+    3.) Create boolean variables r_{i,j} for the row operations, where r_{i,j} is true if the j-th row is added to the i-th row in the auxiliary tableau.
+    4.) Check satisfiability of the resulting formula. If it is satisfiable, then c1 and c2 are local clifford equivalent, otherwise they are not.
+    """
+    return _build_lceq_stab_sat_solver(c1, c2).check() == z3.sat
